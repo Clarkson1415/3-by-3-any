b33edd0bd8634a99c63ffc5aa7b36d7a0c61a2b6
Grace C <72074247+Clarkson1415@users.noreply.github.com>
2020-11-13 20:20:18 +1000

    fixed comments

--- 3 by 3 any/Program.cs
+++ 3 by 3 any/Program.cs
@@ -20,33 +20,25 @@ namespace _3_by_3_any
             int i = 0;
             for (; ; )
             {
-                if (i < 1000000) //run the game until I've won 5 times.
+                if (i < 50) //run the game until I've won 50 times.
                 {
-
-                    //!Console.WriteLine("{0},{1}", x, y); //where u at
-
-
-
-                    if (x == 2 && y == 2) //won
+                    if (x == 2 && y == 2) //check if won
                     {
-                        Console.WriteLine("{0}", r); //!Rolls
                         x = 0;
                         y = 0;
                         r = 0;
                         i++; //increase counter by 1
-
                     }
                     //ladders
                     else if (x == 2 && y == 0)
                     {
                         x = 0;
                         y = 1;
-                        //!Console.WriteLine("up ladder to 2,0");
+                  
                     }
                     //snake
                     else if (x == 0 && y == 2)
                     {
-                        //! Console.WriteLine("snake to 0,2");
                         x = 1;
                         y = 1;
                     }
@@ -54,9 +46,9 @@ namespace _3_by_3_any
                     else
                     {
                         int Roll = diceRoll.Next(1, 4); //roll 1,2 or 3.
-                        //!Console.WriteLine("rolled {0}", Roll);
+                        //Console.WriteLine("rolled {0}", Roll);
                         r++;
-                        //!Console.WriteLine("Times Rolled: {0}", r); //times rolled
+                        //Console.WriteLine("Times Rolled: {0}", r); //uncomment to output times rolled
 
                         if (y % 2 == 0 && y < 2) //if line is even and not the last line
                         {
@@ -103,9 +95,8 @@ namespace _3_by_3_any
                         }
 
 
-                        else if (y % 2 != 0 && y <= 2)
+                        else if (y % 2 != 0 && y <= 2) //if y is not divisible by 2, odd number lines player moves left
                         {
-                            //!Console.WriteLine("y is odd");
                             if (x - Roll >= 0)
                             {
                                 x -= Roll;
